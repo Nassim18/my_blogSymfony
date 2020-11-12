b1330995918f12c9cc11ed8df75e486efdd3bd59
Nassim18 <nassim_sehout@yahoo.fr>
2020-11-12 15:20:03 +0100

    Adding of a new Entity ' Post '

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Post;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,6 +14,13 @@ class BlogController extends AbstractController
      */
     public function index(): Response
     {
+        $post = new Post();
+        $post -> setTitre('Article 1')
+        ->setContent(" içi c'est le contenu de notre article ")
+        ->setUrl_alias(' \url\article ')
+        ->setPublishedAt(new \datetime());
+
+
         return $this->render('blog/index.html.twig', [
 
              'message' => 'Page d\'accueil'
