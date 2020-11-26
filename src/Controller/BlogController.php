0de71c8197264a18b729a3934232e6f4c8aa7891
Amine aminbouali1995@hotmail.com <aminbouali1995@hotmail.com>
2020-11-26 12:08:06 +0100

    Posts added

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -3,37 +3,38 @@
 namespace App\Controller;
 
 use App\Entity\Post;
+use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class BlogController extends AbstractController
-{
+{   public static $ripo;
+
     /**
      * @Route("/blog", name="blog")
+     * @return Response
      */
-    public function index(): Response
+    public function index(PostRepository $ripo): Response
     {
-        $post = new Post();
-        $post -> setTitre('Article 1')
-        ->setContent(" içi c'est le contenu de notre article ")
-        ->setUrl_alias(' \url\article ')
-        ->setPublishedAt(new \datetime());
+        BlogController::$ripo=$ripo;
+        $posts = $ripo->findAll();
 
 
         return $this->render('blog/index.html.twig', [
 
-             'message' => 'Page d\'accueil'
+             'message' => 'Page d\'accueil',
+              'posts'   => $posts
         ]);
     }
 
     /**
      * @Route("/post/{id}", name="post")
      */
-    public function post(String $id): Response
+    public function post(Post $post): Response
     {
-        return $this ->render('blog/post.html.twig',[
-            'id' => $id
+        return $this->render('blog/post.html.twig', [
+            'post' => $post
         ]);
     }
 }
