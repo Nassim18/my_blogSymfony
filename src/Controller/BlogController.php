9d1ab322947f710ba248d3e19d71c3881025169c
Amine aminbouali1995@hotmail.com <aminbouali1995@hotmail.com>
2020-12-27 15:10:21 +0100

    Heroku delete post redirection

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -229,7 +229,6 @@ class BlogController extends AbstractController
         $user = $this->getUser();
         $em->remove($post);
         $em->flush();
-        return $this->redirectToRoute('profile',['username'=>$user->getUsername()]);
     }
 
     /**
@@ -244,7 +243,6 @@ class BlogController extends AbstractController
         $em->remove($comment);
         $post = $this->getDoctrine()->getRepository(Post::class)->findOneBy(['id'=>$comment->getPost()]);
         $em->flush();
-        echo("haw zbi : "+$post->getId());
         return $this->redirectToRoute('post_show',['url_alias' => $post->geturl_alias()]);
     }
 
