dedab6c04a8765f99cd0c62d1b54187416f638e6
Nassim18 <nassim_sehout@yahoo.fr>
2020-12-28 22:17:32 +0100

    delete post fixed

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -241,7 +241,8 @@ class BlogController extends AbstractController
         $post = $this->getDoctrine()->getRepository(Post::class)->findOneBy(['id' => $id]);
         $userConnected = $this->getUser()->getUsername();
         $userr = $this->getDoctrine()->getRepository(User::class)->findOneBy(['id'=> $post->getUser()->getId()]);
-        if(strcmp($userConnected , $userr) !== 0){
+        //dd($userConnected,$userr);
+        if(strcmp($userConnected , $userr->getUsername()) !== 0){
             return $this->render('security/error.html.twig');
         }
         $em = $this->getDoctrine()->getManager();
